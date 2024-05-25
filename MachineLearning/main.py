65df3147b671e93da709b7016a51d6c24fae55c1
elifbolat <99257582+elifbolat@users.noreply.github.com>
2024-05-25 22:06:50 +0300

    Add files via upload

---
+++ MachineLearning/main.py
@@ -0,0 +1,5 @@
+def harf_say(x):
+    return len(x)
+ 
+a = harf_say("Merhaba!")
+#print(a)
---
+++ main.py
@@ -0,0 +1,90 @@
+# Kare işareti yorum satırı demektir.
+# Veri, Fonksiyon, Çıktı
+
+"""
+print("Merhaba Elif")
+
+print(3)
+
+"""
+
+# Print'den çıktı olarka ekrana yazı yazdırmasını bekliyoruz
+
+# Aritmetik Operatorler
+"""
+print(4 + 5)
+print(4 - 5)
+print(4 * 5)
+print(4 / 5)
+"""
+# Değişkenler
+"""
+sayi1 = 10
+sayi2 = 50
+
+print(sayi1 + sayi2)
+print(sayi1 - sayi2)
+print(sayi1 * sayi2)
+print(sayi1 / sayi2)
+"""
+"""
+sayi1 = 10
+sayi2 = 20
+sayi3 = 10
+toplam = sayi1 + sayi2
+cikarma = sayi1 - sayi2
+carpma = sayi1 * sayi2
+bolme = sayi1 / sayi2
+
+print(toplam)
+print(cikarma)
+print(carpma)
+print(bolme)
+"""
+"""
+sayi = 0
+sayi = sayi + 1
+print(sayi)
+"""
+
+# Veri tipleri
+"""
+Binary
+0 ve 1 -> True False (Boolean)
+5 -> 0101
+10 -> 1010
+
+Tam sayi -> Integer
+Ondalikli Sayi -> Float
+Karakter/Yazi -> Char
+Karakter Kombinasyonlari -> String (Karakter dizileri)
+
+"""
+"""
+print(type(3))
+print(type(3.0))
+print(type("Elif"))
+print(type(False))
+"""
+
+"""
+sayi1 = 3
+sayi2 = 0.5
+
+print(type(sayi1))
+print(type(sayi2))
+
+print(type(sayi1+sayi2))
+"""
+sayi1 = input("sayi1'i giriniz->")
+sayi1 = int(sayi1)
+# print(sayi1)
+
+sayi2 = input("sayi2'yi giriniz->")
+sayi2 = int(sayi2)
+# print(sayi2)
+
+print(sayi1+sayi2)
+print(sayi1-sayi2)
+print(sayi1*sayi2)
+print(sayi1/sayi2)
